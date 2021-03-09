7ef66dabe36058c8d67117289fda0dab25f7a95c
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-10 00:33:00 +0300

    Update RegisterViewModel.kt

--- app/src/main/java/com/example/vetapp/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/example/vetapp/viewModel/RegisterViewModel.kt
@@ -2,6 +2,7 @@ package com.example.vetapp.viewModel
 
 import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import com.example.vetapp.Models.UserRegister
 import com.example.vetapp.network.VetAPI
@@ -18,21 +19,25 @@ class RegisterViewModel {
 
     fun userRegister(userName: String, userMailAdres:String, userPassword:String){
         compositeDisposable.add(
-            vetAPI.getService().getRegister(userName,userPassword,userMailAdres).
+            vetAPI.getService().getRegister(userName,userMailAdres,userPassword).
             subscribeOn(Schedulers.newThread()).
             observeOn(AndroidSchedulers.mainThread()).subscribeWith(object :DisposableSingleObserver<UserRegister>(){
                 override fun onSuccess(t: UserRegister) {
                     if(t.tf.toBoolean()){
+
                         t.text
+                        Log.d("text",t.text)
+                        //başarılıysa giriş yapa git
 
                     }else{
                         t.text
+                        Log.d("text",t.text)
                     }
 
                 }
 
                 override fun onError(e: Throwable) {
-                    Log.d("Internet Error","sdfgh")
+                    Log.d("Internet Error","İnternetiniz kontrol ediniz.")
 
                 }
             })
