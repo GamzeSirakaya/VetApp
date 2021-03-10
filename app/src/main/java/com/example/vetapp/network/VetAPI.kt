fa1aba340ce8286d1b0151c0225ec2f5e2138fea
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-11 01:55:00 +0300

    Update VetAPI.kt

--- app/src/main/java/com/example/vetapp/network/VetAPI.kt
+++ app/src/main/java/com/example/vetapp/network/VetAPI.kt
@@ -1,9 +1,8 @@
 package com.example.vetapp.network
 
-import com.example.vetapp.Models.UserLogin
-import com.example.vetapp.Models.UserRegister
+import com.example.vetapp.modelss.UserLogin
+import com.example.vetapp.modelss.UserRegister
 import io.reactivex.Single
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
