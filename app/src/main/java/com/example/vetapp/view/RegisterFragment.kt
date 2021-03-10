d6db1c31697d20c4329b3aafb83ba7d9dfeac223
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-11 01:55:48 +0300

    Update RegisterFragment.kt

--- app/src/main/java/com/example/vetapp/view/RegisterFragment.kt
+++ app/src/main/java/com/example/vetapp/view/RegisterFragment.kt
@@ -1,19 +1,22 @@
 package com.example.vetapp.view
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.example.vetapp.R
+import com.example.vetapp.databinding.FragmentRegisterBinding
+import com.example.vetapp.modelss.UserRegister
 import com.example.vetapp.viewModel.RegisterViewModel
 import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class RegisterFragment : Fragment() {
     val registerViewModel = RegisterViewModel()
+    private lateinit var dataBinding: FragmentRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,34 +27,40 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        dataBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_register, container, false)
+
+        return dataBinding.root
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         registerToUser()
         action()
-       // delete()
+
     }
+
     fun registerToUser() {
         register_button.setOnClickListener {
-            registerViewModel.userRegister(register_name.editableText.toString(),
+
+            registerViewModel.userRegister(
+                register_name.editableText.toString(),
                 register_mail.editableText.toString(),
                 register_password.editableText.toString()
             )
+
+
+          message_text.visibility=View.VISIBLE
         }
     }
-    fun action(){
+
+    fun action() {
         register_text.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.action_registerFragment_to_loginFragment)
         }
     }
-   /* fun delete(){
-        register_name.delete,
-        register_mail.delete,
-        register_password.delete
 
-    }*/
 
 
 }
