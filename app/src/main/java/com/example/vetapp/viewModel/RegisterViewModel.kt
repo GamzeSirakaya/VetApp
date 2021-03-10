8f82eb4b9abb669d25bd792d1e982793cffe333e
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-11 01:55:20 +0300

    Update RegisterViewModel.kt

--- app/src/main/java/com/example/vetapp/viewModel/RegisterViewModel.kt
+++ app/src/main/java/com/example/vetapp/viewModel/RegisterViewModel.kt
@@ -1,10 +1,8 @@
 package com.example.vetapp.viewModel
 
 import android.util.Log
-import android.widget.EditText
-import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
-import com.example.vetapp.Models.UserRegister
+import com.example.vetapp.modelss.UserRegister
 import com.example.vetapp.network.VetAPI
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -12,35 +10,30 @@ import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
 
 class RegisterViewModel {
-    val userRegisterLiveData=MutableLiveData<UserRegister>() //innerclass
-    private val compositeDisposable=CompositeDisposable() //Rxjava2 inner class
-    private val vetAPI=VetAPI
+    val userRegisterLiveData = MutableLiveData<UserRegister>() //innerclass
+    private val compositeDisposable = CompositeDisposable() //Rxjava2 inner class
+    private val vetAPI = VetAPI
 
 
-    fun userRegister(userName: String, userMailAdres:String, userPassword:String){
+    fun userRegister(userName: String, userMailAdres: String, userPassword: String) {
         compositeDisposable.add(
-            vetAPI.getService().getRegister(userName,userMailAdres,userPassword).
-            subscribeOn(Schedulers.newThread()).
-            observeOn(AndroidSchedulers.mainThread()).subscribeWith(object :DisposableSingleObserver<UserRegister>(){
-                override fun onSuccess(t: UserRegister) {
-                    if(t.tf.toBoolean()){
+            vetAPI.getService().getRegister(userName, userMailAdres, userPassword)
+                .subscribeOn(Schedulers.newThread()).observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<UserRegister>() {
+                    override fun onSuccess(t: UserRegister) {
 
-                        t.text
-                        Log.d("text",t.text)
-                        //başarılıysa giriş yapa git
+                            userRegisterLiveData.value = t
+                            Log.d("text", t.text)
 
-                    }else{
-                        t.text
-                        Log.d("text",t.text)
+
+
                     }
 
-                }
+                    override fun onError(e: Throwable) {
+                        Log.d("Internet Error", "İnternetiniz kontrol ediniz.")
 
-                override fun onError(e: Throwable) {
-                    Log.d("Internet Error","İnternetiniz kontrol ediniz.")
-
-                }
-            })
+                    }
+                })
         )
 
     }
