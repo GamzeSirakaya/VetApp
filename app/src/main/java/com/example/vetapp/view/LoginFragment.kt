33991750f3436805c23956e2ae49b2c024cd914c
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-11 16:31:49 +0300

    Update LoginFragment.kt

--- app/src/main/java/com/example/vetapp/view/LoginFragment.kt
+++ app/src/main/java/com/example/vetapp/view/LoginFragment.kt
@@ -2,24 +2,29 @@ package com.example.vetapp.view
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.makeText
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.example.vetapp.R
 import com.example.vetapp.databinding.FragmentLoginBinding
+import com.example.vetapp.modelss.UserLogin
 import com.example.vetapp.viewModel.LoginViewModel
 import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_register.*
 import kotlinx.android.synthetic.main.fragment_register.register_text
+import java.util.Observer
 
 
 class LoginFragment : Fragment() {
     val loginViewModel = LoginViewModel()
     private lateinit var dataBinding: FragmentLoginBinding
-
+    private lateinit var userLogin: UserLogin
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,14 +35,17 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
         // Inflate the layout for this fragment
-        return dataBinding.root
+
+
+        return inflater.inflate( R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         action()
+        observerLiveData()
     }
 
     fun action() {
@@ -50,10 +58,18 @@ class LoginFragment : Fragment() {
                 login_mail.editableText.toString(),
                 login_password.editableText.toString()
             )
-        messagelogin_text.visibility = View.VISIBLE
-
         }
     }
 
+    fun observerLiveData() {
+        loginViewModel.userLoginLiveData.observe(viewLifecycleOwner, androidx.lifecycle.Observer {it.let {
+            userLogin -> userLogin
+            Toast.makeText(context,it.text,Toast.LENGTH_LONG).show()
+        }
 
+
+        })
+
+
+    }
 }
