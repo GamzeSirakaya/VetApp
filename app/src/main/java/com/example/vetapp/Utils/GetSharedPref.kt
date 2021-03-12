23c33ef819dda070fa374158e04590f506d2e718
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-12 23:45:53 +0300

    Update GetSharedPref.kt

--- app/src/main/java/com/example/vetapp/Utils/GetSharedPref.kt
+++ app/src/main/java/com/example/vetapp/Utils/GetSharedPref.kt
@@ -3,9 +3,25 @@ package com.example.vetapp.Utils
 import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
+import androidx.fragment.app.Fragment
+import com.example.vetapp.view.LoginFragment
+import com.example.vetapp.view.MainActivity
 
 
-object GetSharedPref{
+object GetSharedPref {
+    lateinit var sharedPreferences: SharedPreferences
+    fun init(activity: Activity) {
+
+        sharedPreferences = activity.getSharedPreferences("pref", Context.MODE_PRIVATE)
+    }
+
+    fun save(id: Int, name: String, mail: String) {
+        sharedPreferences.edit().putInt("id", id).apply()
+        sharedPreferences.edit().putString("name", name).apply()
+        sharedPreferences.edit().putString("mail", mail).apply()
+
+    }
+
 
 
 }
