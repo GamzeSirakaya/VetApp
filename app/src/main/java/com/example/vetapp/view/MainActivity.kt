f455c92e722555a2cac94481d892deecc89c72c3
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-12 21:19:26 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/vetapp/view/MainActivity.kt
+++ app/src/main/java/com/example/vetapp/view/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.vetapp.view
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.vetapp.R
+import com.example.vetapp.Utils.GetSharedPref
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_register.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        }
+
     }
-}
+
+
+
