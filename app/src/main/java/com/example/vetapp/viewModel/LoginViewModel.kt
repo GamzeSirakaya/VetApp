a057059210b79f4a269b7653648bfec985435968
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-12 21:19:32 +0300

    Update LoginViewModel.kt

--- app/src/main/java/com/example/vetapp/viewModel/LoginViewModel.kt
+++ app/src/main/java/com/example/vetapp/viewModel/LoginViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.vetapp.viewModel
 
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
+import com.example.vetapp.Utils.GetSharedPref
 import com.example.vetapp.modelss.UserLogin
-import com.example.vetapp.modelss.UserRegister
 import com.example.vetapp.network.VetAPI
+import com.example.vetapp.view.LoginFragment
+import com.example.vetapp.view.MainActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
@@ -21,9 +24,8 @@ class LoginViewModel {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<UserLogin>() {
                     override fun onSuccess(t: UserLogin) {
-                       userLoginLiveData.value=t
-                        Log.d("text",t.text)
-
+                        userLoginLiveData.value = t
+                        //GetSharedPref.save(t.id,t.kadi,t.mailAdres)
                     }
 
                     override fun onError(e: Throwable) {
