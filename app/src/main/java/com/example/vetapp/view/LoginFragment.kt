55270a1e3325b1559f7f2caee692233f406bd0cf
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-13 23:37:17 +0300

    Update LoginFragment.kt

--- app/src/main/java/com/example/vetapp/view/LoginFragment.kt
+++ app/src/main/java/com/example/vetapp/view/LoginFragment.kt
@@ -1,5 +1,6 @@
 package com.example.vetapp.view
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.vetapp.R
+import com.example.vetapp.Utils.GetSharedPref
 import com.example.vetapp.databinding.FragmentLoginBinding
 import com.example.vetapp.modelss.UserLogin
 import com.example.vetapp.viewModel.LoginViewModel
@@ -16,9 +18,8 @@ import kotlinx.android.synthetic.main.fragment_register.*
 
 
 class LoginFragment : Fragment() {
-
+    lateinit var sharedPreferences: SharedPreferences
     val loginViewModel = LoginViewModel()
-    private lateinit var dataBinding: FragmentLoginBinding
     private lateinit var userLogin: UserLogin
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,7 +35,7 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
 
 
-        return inflater.inflate( R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -58,19 +59,26 @@ class LoginFragment : Fragment() {
             Navigation.findNavController(it).navigate(R.id.action_loginFragment_to_homeFragment)
         }
     }
-    fun delete()
-    {
+
+    fun delete() {
         login_mail.setText("")
         login_password.setText("")
     }
+
     fun observerLiveData() {
-        loginViewModel.userLoginLiveData.observe(viewLifecycleOwner, androidx.lifecycle.Observer {it.let {
-            userLogin -> userLogin
-            Toast.makeText(context,it.text,Toast.LENGTH_LONG).show()
+        loginViewModel.userLoginLiveData.observe(viewLifecycleOwner, androidx.lifecycle.Observer {
+            it.let { userLogin ->
+                userLogin
+                Toast.makeText(context, it.text, Toast.LENGTH_LONG).show()
+
+                /*context?.let { it1 -> GetSharedPref.init(it1) }
+
+                sharedPreferences.getInt("id", id)
+                sharedPreferences.getString("name", register_name.editableText.toString())
+                sharedPreferences.getString("mail", login_mail.editableText.toString())*/
 
 
-
-        }
+            }
 
 
         })
