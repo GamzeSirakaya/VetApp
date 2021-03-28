1c361896d759d8b4c01d539c252e151c67d77704
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-29 01:47:51 +0300

    Update UserPetsFragment.kt

--- app/src/main/java/com/example/vetapp/view/UserPetsFragment.kt
+++ app/src/main/java/com/example/vetapp/view/UserPetsFragment.kt
@@ -1,16 +1,20 @@
 package com.example.vetapp.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.vetapp.R
 import com.example.vetapp.adapter.PetListAdapter
 import com.example.vetapp.viewModel.UserPetsViewModel
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_user_pets.*
 
 
@@ -29,30 +33,50 @@ class UserPetsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_user_pets, container, false)
 
-        // Inflate the layout for this fragment
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        petsViewModel=ViewModelProviders.of(this).get(UserPetsViewModel::class.java)
+        petsViewModel = ViewModelProviders.of(this).get(UserPetsViewModel::class.java)
         petsViewModel.refreshData()
         setupRecycler()
         observeLiveData()
+
+
     }
 
     fun setupRecycler() {
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerView.adapter = recyclerAdapter
+        petRecyclerView.layoutManager = LinearLayoutManager(context)
+        petRecyclerView.adapter = recyclerAdapter
+
+
     }
 
     fun observeLiveData() {
-        petsViewModel.petListLiveData.observe(viewLifecycleOwner, Observer {petList->petList?.let {
-            recyclerAdapter.update(it)
-        }
+        petsViewModel.petListLiveData.observe(viewLifecycleOwner, Observer {
+            if (it.get(0).tf) {
+               val petlist= it
+                recyclerAdapter.update(petlist)
+                Toast.makeText(
+                    context,
+                    "Sistemde Kayıtlı "+petlist.size+" bulunmamaktadır.",
+                    Toast.LENGTH_LONG
+                )
+            } else {
+                Toast.makeText(
+                    context,
+                    "Sistemde Kayıtlı petiniz bulunmamaktadır.",
+                    Toast.LENGTH_LONG
+                )
+
+            }
+
+
 
         })
     }
+
 
 }
 
