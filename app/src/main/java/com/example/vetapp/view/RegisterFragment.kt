b7b885f27b25bcb2216737420e5dfe81bf969f4d
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-29 01:47:24 +0300

    Update RegisterFragment.kt

--- app/src/main/java/com/example/vetapp/view/RegisterFragment.kt
+++ app/src/main/java/com/example/vetapp/view/RegisterFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
 import com.example.vetapp.R
 import com.example.vetapp.viewModel.RegisterViewModel
@@ -14,7 +15,7 @@ import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
 
-    val registerViewModel = RegisterViewModel()
+ private lateinit var registerViewModel :RegisterViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +34,7 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        registerViewModel = ViewModelProviders.of(this).get(RegisterViewModel::class.java)
         registerToUser()
         action()
         observerLiveData()
