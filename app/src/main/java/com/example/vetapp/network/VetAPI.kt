7f909f0e1c39bf376b41f39844787e5e782c1ec8
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-05-06 23:50:33 +0300

    Update VetAPI.kt

--- app/src/main/java/com/example/vetapp/network/VetAPI.kt
+++ app/src/main/java/com/example/vetapp/network/VetAPI.kt
@@ -1,8 +1,10 @@
 package com.example.vetapp.network
 
 import com.example.vetapp.model.PetList
+import com.example.vetapp.model.Question
 import com.example.vetapp.model.UserLogin
 import com.example.vetapp.model.UserRegister
+import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -11,23 +13,34 @@ import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
+
 interface VetAPI {
     @FormUrlEncoded
     @POST("kayitol.php")
-    fun getRegister(@Field("kadi") kadi:String,
-                    @Field("mailAdres") mailAdres: String,
-                    @Field("parola") parola:String):Single<UserRegister>
+    fun getRegister(
+        @Field("kadi") kadi: String,
+        @Field("mailAdres") mailAdres: String,
+        @Field("parola") parola: String
+    ): Single<UserRegister>
+
     @FormUrlEncoded
     @POST("girisyap.php")
-    fun getLogin(@Field("mail") mailAdres: String,
-                 @Field("password") password:String):Single<UserLogin>
+    fun getLogin(
+        @Field("mail") mailAdres: String,
+        @Field("password") password: String
+    ): Single<UserLogin>
 
     @FormUrlEncoded
     @POST("petlerim.php")
-    fun getPetList(@Field("musid") mus_id: String,
-    ):Single<List<PetList>>
+    fun getPetList(
+        @Field("musid") mus_id: String,
+    ): Observable<List<PetList>>
 
-    companion object{
+    @FormUrlEncoded
+    @POST("sorusor.php")
+    fun getQuestion(@Field("id") id: String, @Field("soru") soru: String): Observable<Question>
+
+    companion object {
         const val BASE_URL = "https://www.gamzesirakaya.com/veterinerservis/"
         fun getService(): VetAPI {
             val retrofit = Retrofit.Builder()
