a099146a4d8c7487b133be5de0c51209cfab2deb
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-05-20 23:00:54 +0300

    Update PetListAdapter.kt

--- app/src/main/java/com/example/vetapp/adapter/PetListAdapter.kt
+++ app/src/main/java/com/example/vetapp/adapter/PetListAdapter.kt
@@ -2,40 +2,47 @@ package com.example.vetapp.adapter
 
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.vetapp.R
 import com.example.vetapp.databinding.RecyclerItemBinding
 import com.example.vetapp.model.PetList
+import kotlinx.android.synthetic.main.recycler_item.view.*
 
 class PetListAdapter(var petlist: ArrayList<PetList>) :
     RecyclerView.Adapter<PetListAdapter.PetViewHolder>() {
 
-    class PetViewHolder(var view: RecyclerItemBinding) : RecyclerView.ViewHolder(view.root) {
+    class PetViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
 
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = DataBindingUtil.inflate<RecyclerItemBinding>(
-            inflater,
+        val view = inflater.inflate(
             R.layout.recycler_item,
             parent,
             false
         )
+
         return PetViewHolder(view)
     }
+
     override fun getItemCount(): Int {
         return petlist.size
 
     }
-    override fun onBindViewHolder(holder: PetViewHolder, position: Int) {
-        holder.view.petlistt=petlist[position]
-        holder.view.executePendingBindings()
-    }
 
+    override fun onBindViewHolder(holder: PetViewHolder, position: Int) {
+        holder.itemView.apply {
+            petisim.text = petlist[position].petIsim
+            petCins.text = petlist[position].petCins
+            Glide.with(this).load(petlist[position].petResim).into(petResim)
+        }
+    }
 
 
     fun update(newList: List<PetList>) {
