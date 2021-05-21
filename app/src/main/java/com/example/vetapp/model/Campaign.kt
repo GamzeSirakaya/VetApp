d3f750a28f170d0246bc9ff2f79f0c760291c09d
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-05-22 00:59:05 +0300

    Create Campaign.kt

---
+++ app/src/main/java/com/example/vetapp/model/Campaign.kt
@@ -0,0 +1,12 @@
+package com.example.vetapp.model
+
+data class Campaign(
+    val resim: String? = null,
+    val tf: Boolean? = null,
+    val id: String? = null,
+    val text: String? = null,
+    val baslik: String? = null
+)
+
+
+
