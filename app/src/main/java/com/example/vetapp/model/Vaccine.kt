2c7c1430d40f4e8ebd7897564a77de50204c7bc8
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-05-23 00:02:23 +0300

    Create Vaccine.kt

---
+++ app/src/main/java/com/example/vetapp/model/Vaccine.kt
@@ -0,0 +1,12 @@
+package com.example.vetapp.model
+
+data class Vaccine(
+	val tf: Any? = null,
+	val petresim: Any? = null,
+	val pettur: Any? = null,
+	val asitarih: Any? = null,
+	val petisim: Any? = null,
+	val petcins: Any? = null,
+	val asiismi: Any? = null
+)
+
